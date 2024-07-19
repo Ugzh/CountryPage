ad345be79251aed463f457810a5808f744b8c97e
Ugzher <ugo.mateda@gmail.com>
2024-07-19 13:39:50 +0200

    Create side filter

---
+++ src/app/components/filter/SideFilter.tsx
@@ -0,0 +1,11 @@
+import SortFilter from "@/app/components/filter/SortFilter";
+
+function SideFilter() {
+  return (
+    <div className="flex flex-col">
+      <SortFilter></SortFilter>
+    </div>
+  );
+}
+
+export default SideFilter;
